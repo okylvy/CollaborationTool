e41d4048a8eab0ecf77825bae4cefbe53e689d64
okylvy <okylvy@yahoo.co.jp>
2021-03-06 15:33:35 +0900

    Initial Commit

---
+++ CollaborationTool/CollaborationToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollaborationToolApp.swift
+//  CollaborationTool
+//
+//  Created by hiroki on 2021/03/06.
+//
+
+import SwiftUI
+
+@main
+struct CollaborationToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
